4eaed728a7ded3124e4316f78c5272420a973b00
Dominik <dominiks8318@gmail.com>
2023-07-28 15:34:50 +0200

    Added DurationUtil tests.

---
+++ doublejump-core/src/test/java/DurationUtilTest.java
@@ -0,0 +1,49 @@
+import me.dmk.doublejump.util.DurationUtil;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DurationUtilTest {
+
+    @Test
+    void testSeconds() {
+        Duration ofSeconds = Duration.ofSeconds(5);
+
+        String excepted = "5s";
+        String result = DurationUtil.toHumanReadable(ofSeconds);
+
+        assertEquals(excepted, result);
+    }
+
+    @Test
+    void testMinutes() {
+        Duration ofMinutes = Duration.ofMinutes(5);
+
+        String excepted = "5m";
+        String result = DurationUtil.toHumanReadable(ofMinutes);
+
+        assertEquals(excepted, result);
+    }
+
+    @Test
+    void testHours() {
+        Duration ofHours = Duration.ofHours(10);
+
+        String excepted = "10h";
+        String result = DurationUtil.toHumanReadable(ofHours);
+
+        assertEquals(excepted, result);
+    }
+
+    @Test
+    void testDays() {
+        Duration ofDays = Duration.ofDays(30);
+
+        String excepted = "720h";
+        String result = DurationUtil.toHumanReadable(ofDays);
+
+        assertEquals(excepted, result);
+    }
+}
