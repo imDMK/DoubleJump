948619f1b46344d10deb5191befbc46034071f0f
Dominik <dominiks8318@gmail.com>
2023-10-18 11:52:19 +0200

    Code fixes.

--- doublejump-api/src/main/java/com/github/imdmk/doublejump/DoubleJumpApiProvider.java
+++ doublejump-api/src/main/java/com/github/imdmk/doublejump/DoubleJumpApiProvider.java
@@ -2,26 +2,33 @@ package com.github.imdmk.doublejump;
 
 public final class DoubleJumpApiProvider {
 
-    private static DoubleJumpApi api;
+    private static DoubleJumpApi DOUBLE_JUMP_API;
 
     private DoubleJumpApiProvider() {
         throw new UnsupportedOperationException("This class cannot be instantiated.");
     }
 
     public static DoubleJumpApi get() {
-        DoubleJumpApi doubleJumpApi = DoubleJumpApiProvider.api;
-        if (doubleJumpApi == null) {
+        if (DOUBLE_JUMP_API == null) {
             throw new IllegalStateException("The DoubleJump API isn't registered.");
         }
 
-        return doubleJumpApi;
+        return DOUBLE_JUMP_API;
     }
 
     static void register(DoubleJumpApi doubleJumpApi) {
-        DoubleJumpApiProvider.api = doubleJumpApi;
+        if (DOUBLE_JUMP_API != null) {
+            throw new IllegalStateException("The DoubleJump API is already registered.");
+        }
+
+        DOUBLE_JUMP_API = doubleJumpApi;
     }
 
     static void unregister() {
-        DoubleJumpApiProvider.api = null;
+        if (DOUBLE_JUMP_API == null) {
+            throw new IllegalStateException("The DoubleJump API isn't registered.");
+        }
+
+        DOUBLE_JUMP_API = null;
     }
 }
