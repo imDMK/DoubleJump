84575085d8bb6a0f6dfb18680673a454932274e6
Dominik <dominiks8318@gmail.com>
2024-03-05 13:20:45 +0100

    Fix jitpack dependency.

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+allprojects {
+    apply(plugin="java-library")
+    apply(plugin="maven-publish")
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+
+                from(components["java"])
+            }
+        }
+    }
+}
