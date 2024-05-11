672c619bcd629cdebe2c3537277405b2c7a9053f
DMK <81445555+imDMK@users.noreply.github.com>
2024-05-11 11:57:19 +0200

    Fix build

--- doublejump-api/build.gradle.kts
+++ doublejump-api/build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
     mavenLocal()
 
     maven("https://oss.sonatype.org/content/repositories/central")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    
     maven("https://maven.enginehub.org/repo/")
 }
 
